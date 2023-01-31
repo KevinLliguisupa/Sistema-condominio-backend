7049ded4d37519d3cbbf883fd2680a0bfa785414
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2023-01-30 23:36:56 -0500

    Proyecto inicial Spring Boot

---
+++ src/main/java/com/restapi/siscondominio/SiscondominioApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.siscondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscondominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscondominioApplication.class, args);
+	}
+
+}
