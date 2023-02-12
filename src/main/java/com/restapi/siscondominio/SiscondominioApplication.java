cc43fd1acbe21362e29406b7ad095be5b4beb020
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2023-02-11 21:28:53 -0500

    Unificacion de Script DB y agregar CORS

--- src/main/java/com/restapi/siscondominio/SiscondominioApplication.java
+++ src/main/java/com/restapi/siscondominio/SiscondominioApplication.java
@@ -2,8 +2,11 @@ package com.restapi.siscondominio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -12,6 +15,18 @@ public class SiscondominioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SiscondominioApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
+			}
+		};
+	}
+
 	@GetMapping
 	public String greeting(){
 		return "Bienvenido al Rest API SisCondominio V1";
