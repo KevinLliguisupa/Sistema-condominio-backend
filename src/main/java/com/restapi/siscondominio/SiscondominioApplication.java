c259565388a11501726cf8f4cd5127bb68121218
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2023-02-21 20:26:44 -0500

    Feat configuracion: Preparacion para deploy 1

--- src/main/java/com/restapi/siscondominio/SiscondominioApplication.java
+++ src/main/java/com/restapi/siscondominio/SiscondominioApplication.java
@@ -27,7 +27,7 @@ public class SiscondominioApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/login")
-								.allowedOrigins("http://localhost:4200")
+								.allowedOrigins("/**")
 										.allowedMethods("*")
 												.exposedHeaders("*");
 
